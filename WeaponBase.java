912cbcebe75f49323fec9a8be5a4e6323b8fcb43
Mattia Andreolli <mattia.andreolli1@gmail.com>
2022-11-16 15:54:42 +0100

    feat: Added Weapon attributes

--- WeaponBase.java
+++ WeaponBase.java
@@ -1,6 +1,31 @@
 /*
  * Base class for all weapons
  */
-public abstract class WeaponBase
+public class WeaponBase
 {
+    private String name;    //Name of the weapon
+    private float stat; //Stat value of the weapon
+    private WeaponType type;    //Type of the weapon
+
+    public WeaponBase(String name, float stat, WeaponType type)
+    {
+        this.name = name;
+        this.stat = stat;
+        this.type = type;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public float getStat()
+    {
+        return stat;
+    }
+
+    public WeaponType getType()
+    {
+        return type;
+    }
 }
